926bf04f9d070cfd8f00853dd99a6f6125634e2f
myTerminal <team.terminal@gmail.com>
2017-10-02 19:57:43 -0500

    initial implementation

--- src/scripts/app.jsx
+++ src/scripts/app.jsx
@@ -4,13 +4,59 @@ import '../styles/styles.less';
 
 const React = require('react');
 const ReactDOM = require('react-dom');
+const org = require('org');
 
 const page = document.getElementById('page');
 
-var Test = React.createClass({
-    render: function () {
-        return <div className='test'>This is a test!</div>;
-    }
-});
+class App extends React.Component {
+    constructor() {
+        super();
 
-ReactDOM.render(<Test />, page);
+        this.state = {
+            transpiledText: 'Paste an org document on the left to view it in HTML form here'
+        };
+
+        this.parser = new org.Parser();
+    }
+
+    handleChange(e) {
+        var input = e.target.value;
+
+        this.setState(() => ({
+            transpiledText: this.parser
+                .parse(input)
+                .convert(org.ConverterHTML, {
+                    headerOffset: 0
+                })
+                .toString()
+        }));
+    }
+
+    render() {
+        return (
+            <div className="container">
+              <Input onChange={this.handleChange.bind(this)} />
+              <Output text={this.state.transpiledText} />
+            </div>
+        );
+    }
+}
+
+class Input extends React.Component {
+    render() {
+        return <textarea className="input" onChange={this.props.onChange} />;
+    }
+}
+
+class Output extends React.Component {
+    render() {
+        return (
+            <div
+              className="output"
+              dangerouslySetInnerHTML={{ __html: this.props.text }}
+              />
+        );
+    }
+}
+
+ReactDOM.render(<App />, page);
