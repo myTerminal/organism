3bbc208c02ecf6147a860083b5a426f97a42bcd7
Mohammed Ismail Ansari <team.terminal@gmail.com>
2017-10-10 14:20:08 +0000

    remove repetition from webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -70,19 +70,7 @@ module.exports = {
                 })
             },
             {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            'babel-preset-env'
-                        ]
-                    }
-                }
-            },
-            {
-                test: /\.jsx$/,
+                test: /\.(jsx|js)$/,
                 exclude: /(node_modules|bower_components)/,
                 use: {
                     loader: 'babel-loader',
