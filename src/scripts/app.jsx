f203ec2ae4861898f4d91b721b2f3d4b14c09b40
myTerminal <team.terminal@gmail.com>
2017-11-19 17:26:42 -0600

    enable transpilation of markdown

--- src/scripts/app.jsx
+++ src/scripts/app.jsx
@@ -5,38 +5,61 @@ import '../styles/styles.less';
 const React = require('react');
 const ReactDOM = require('react-dom');
 const org = require('org');
+const markdown = require('markdown').markdown;
 
 const page = document.getElementById('page');
+
+const defaultTranspiledText = 'Type text or paste a document on the left to view it in HTML form here';
 
 class App extends React.Component {
     constructor() {
         super();
 
-        this.state = {
-            transpiledText: 'Paste an org document on the left to view it in HTML form here'
+        this.transforms = {
+            'Org': function (text) {
+                return this.orgParser
+                    .parse(text)
+                    .convert(org.ConverterHTML, {
+                        headerOffset: 0
+                    })
+                    .toString();
+            },
+            'Markdown': function (text) {
+                return markdown.toHTML(text);
+            }
         };
 
-        this.parser = new org.Parser();
+        this.orgParser = new org.Parser();
+
+        this.state = {
+            selectedTransform: 'Org',
+            transpiledText: defaultTranspiledText
+        };
     }
 
-    handleChange(e) {
+    handleTextChange(e) {
         var input = e.target.value;
 
         this.setState(() => ({
-            transpiledText: this.parser
-                .parse(input)
-                .convert(org.ConverterHTML, {
-                    headerOffset: 0
-                })
-                .toString()
+            transpiledText: this.transforms[this.state.selectedTransform].bind(this)(input)
+        }));
+    }
+
+    handleTransformChange(e) {
+        var selectedTransform = e.target.value;
+
+        this.setState(() => ({
+            selectedTransform: selectedTransform,
+            transpiledText: defaultTranspiledText
         }));
     }
 
     render() {
         return (
             <div className="container">
-              <Input onChange={this.handleChange.bind(this)} />
+              <Input onChange={this.handleTextChange.bind(this)} />
               <Output text={this.state.transpiledText} />
+              <Selector transforms={this.transforms} selectedTransform={this.state.selectedTransform} onChange={this.handleTransformChange.bind(this)} />
             </div>
         );
     }
@@ -55,6 +78,18 @@ class Output extends React.Component {
               className="output"
               dangerouslySetInnerHTML={{ __html: this.props.text }}
               />
+        );
+    }
+}
+
+class Selector extends React.Component {
+    render() {
+        return (
+            <select className="transform-selector" value={this.props.selectedTransform} onChange={this.props.onChange}>
+              {
+                  Object.keys(this.props.transforms).map(k => <option key={k} value={k}>{k}</option>)
+            }
+            </select>
         );
     }
 }
