8c8f8f9229a0dfb9dda106db9fcca7063ec6a45f
Mohammed Ismail Ansari <team.terminal@gmail.com>
2018-04-23 21:14:57 +0000

    fix eslint issues

--- src/scripts/app.jsx
+++ src/scripts/app.jsx
@@ -1,34 +1,39 @@
-/* global require */
+/* global require document */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import '../styles/styles.less';
 import './service-worker-starter.js';
-
-const React = require('react');
-const ReactDOM = require('react-dom');
-
-const org = require('org');
-const markdown = new (require('showdown')).Converter();
-const rst2mdown = require('rst2mdown');
-const textilejs = require('textile-js');
-// const asciidoctor = require('asciidoctor.js')();
-const creole = new (require('npm-creole'))();
-const BBCodeParser = require('bbcode-parser');
-const bbcodeParser = new BBCodeParser(BBCodeParser.defaultTags());
-
 import packageDetails from '../../package.json';
-
 import samples from './samples.js';
 
-const page = document.getElementById('page');
+const org = require('org');
 
-const defaultTranspiledText = 'Type text or paste a document on the left to view it in HTML form here';
+const showdown = require('showdown');
+
+const markdown = new showdown.Converter();
+
+const rst2mdown = require('rst2mdown');
+
+const textilejs = require('textile-js');
+
+// const asciidoctor = require('asciidoctor.js')();
+
+const creole = new (require('npm-creole'))();
+
+const BBCodeParser = require('bbcode-parser');
+
+const bbcodeParser = new BBCodeParser(BBCodeParser.defaultTags());
+
+const page = document.getElementById('page');
 
 class App extends React.Component {
     constructor() {
         super();
 
         this.transforms = {
-            'Org': function (text) {
+            Org: function (text) {
                 return this.orgParser
                     .parse(text)
                     .convert(org.ConverterHTML, {
@@ -36,22 +41,22 @@ class App extends React.Component {
                     })
                     .toString();
             },
-            'Markdown': function (text) {
+            Markdown: function (text) {
                 return markdown.makeHtml(text);
             },
-            'ReStructuredText': function (text) {
+            ReStructuredText: function (text) {
                 return markdown.makeHtml(rst2mdown(text));
             },
-            'TxStyle': function (text) {
+            TxStyle: function (text) {
                 return textilejs(text);
             },
-            // 'AsciiDoc': function (text) {
+            // AsciiDoc: function (text) {
             //     return asciidoctor.convert(text);
             // }
-            'Creole': function (text) {
+            Creole: function (text) {
                 return creole.parse(text);
             },
-            'BBCode': function (text) {
+            BBCode: function (text) {
                 return bbcodeParser.parseString(text);
             }
         };
@@ -59,9 +64,9 @@ class App extends React.Component {
         this.orgParser = new org.Parser();
 
         this.state = {
-            inputText: samples['Org'],
+            inputText: samples.Org,
             selectedTransform: 'Org',
-            transpiledText: this.transforms['Org'].bind(this)(samples['Org'])
+            transpiledText: this.transforms.Org.bind(this)(samples.Org)
         };
     }
 
@@ -80,21 +85,26 @@ class App extends React.Component {
         this.setState(() => ({
             inputText: samples[selectedTransform],
             selectedTransform: selectedTransform,
-            transpiledText: this.transforms[selectedTransform].bind(this)(samples[selectedTransform])
+            transpiledText: this.transforms[selectedTransform]
+                .bind(this)(samples[selectedTransform])
         }));
     }
 
     render() {
         return (
-            <div className='root-container'>
-                <div className='header'>
-                    <b>organism</b> - A live-preview editor for org and more (v{packageDetails.version})
-                    <a className='source' href='https://github.com/myTerminal/organism' target='_blank'></a>
+            <div className="root-container">
+                <div className="header">
+                    <b>organism</b>
+                    - A live-preview editor for org and more (v{packageDetails.version})
+                    <a className="source" href="https://github.com/myTerminal/organism" target="_blank">&nbsp;</a>
                 </div>
                 <div className="container">
-                    <Input text={this.state.inputText} onChange={this.handleTextChange.bind(this)} />
+                    <Input text={this.state.inputText}
+                        onChange={this.handleTextChange.bind(this)} />
                     <Output text={this.state.transpiledText} />
-                    <Selector transforms={this.transforms} selectedTransform={this.state.selectedTransform} onChange={this.handleTransformChange.bind(this)} />
+                    <Selector transforms={this.transforms}
+                        selectedTransform={this.state.selectedTransform}
+                        onChange={this.handleTransformChange.bind(this)} />
                 </div>
             </div>
         );
@@ -105,9 +115,9 @@ class Input extends React.Component {
     render() {
         return (
             <textarea className="input"
-                      value={this.props.text}
-                      onChange={this.props.onChange}
-                      />
+                value={this.props.text}
+                onChange={this.props.onChange}
+            />
         );
     }
 }
@@ -115,10 +125,9 @@ class Input extends React.Component {
 class Output extends React.Component {
     render() {
         return (
-            <div
-              className="output"
-              dangerouslySetInnerHTML={{__html: this.props.text}}
-              />
+            <div className="output"
+                dangerouslySetInnerHTML={{ __html: this.props.text }}
+            />
         );
     }
 }
@@ -126,10 +135,13 @@ class Output extends React.Component {
 class Selector extends React.Component {
     render() {
         return (
-            <select className="transform-selector" value={this.props.selectedTransform} onChange={this.props.onChange}>
-              {
-                  Object.keys(this.props.transforms).map(k => <option key={k} value={k}>{k}</option>)
-            }
+            <select className="transform-selector"
+                value={this.props.selectedTransform}
+                onChange={this.props.onChange}>
+                {
+                    Object.keys(this.props.transforms)
+                        .map(k => <option key={k} value={k}>{k}</option>)
+                }
             </select>
         );
     }
--- src/scripts/samples.js
+++ src/scripts/samples.js
@@ -1,5 +1,5 @@
 export default {
-    'Org': `Sample org document
+    Org: `Sample org document
 
 *Lorem ipsum* dolor sit amet, consectetur adipiscing elit. Nunc vitae lorem molestie, tristique diam at, egestas neque. Nam a interdum urna, eget sodales quam. Vestibulum venenatis libero id dui iaculis sagittis. Vestibulum vel iaculis justo, in posuere ligula. In lacinia massa ac ultricies pretium. Phasellus hendrerit consectetur leo, non pharetra quam hendrerit vel. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque id sem maximus, tempor massa eu, gravida turpis. Aliquam nec risus semper dui vulputate commodo eu vitae massa. In fringilla nulla vel tristique porta.
 
@@ -17,7 +17,7 @@ export default {
 2. Nullam eget neque quis justo euismod eleifend.
 3. Nullam quis turpis porttitor, elementum ex vitae, ultrices arcu.
 4. Aliquam imperdiet ex ut purus tincidunt pretium.`,
-    'Markdown': `# Sample Markdown document
+    Markdown: `# Sample Markdown document
 
 **Lorem ipsum** dolor sit amet, consectetur adipiscing elit. Nunc vitae lorem molestie, tristique diam at, egestas neque. Nam a interdum urna, eget sodales quam. Vestibulum venenatis libero id dui iaculis sagittis. Vestibulum vel iaculis justo, in posuere ligula. In lacinia massa ac ultricies pretium. Phasellus hendrerit consectetur leo, non pharetra quam hendrerit vel. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque id sem maximus, tempor massa eu, gravida turpis. Aliquam nec risus semper dui vulputate commodo eu vitae massa. In fringilla nulla vel tristique porta.
 
@@ -35,7 +35,7 @@ export default {
 2. Nullam eget neque quis justo euismod eleifend.
 3. Nullam quis turpis porttitor, elementum ex vitae, ultrices arcu.
 4. Aliquam imperdiet ex ut purus tincidunt pretium.`,
-    'ReStructuredText': `================================
+    ReStructuredText: `================================
 Sample ReStructuredText document
 ================================
 
@@ -59,7 +59,7 @@ Sed vel ultrices ipsum
 2. Nullam eget neque quis justo euismod eleifend.
 3. Nullam quis turpis porttitor, elementum ex vitae, ultrices arcu.
 4. Aliquam imperdiet ex ut purus tincidunt pretium.`,
-    'TxStyle': `h1. Sample TxStyle document
+    TxStyle: `h1. Sample TxStyle document
 
 *Lorem ipsum* dolor sit amet, consectetur adipiscing elit. Nunc vitae lorem molestie, tristique diam at, egestas neque. Nam a interdum urna, eget sodales quam. Vestibulum venenatis libero id dui iaculis sagittis. Vestibulum vel iaculis justo, in posuere ligula. In lacinia massa ac ultricies pretium. Phasellus hendrerit consectetur leo, non pharetra quam hendrerit vel. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque id sem maximus, tempor massa eu, gravida turpis. Aliquam nec risus semper dui vulputate commodo eu vitae massa. In fringilla nulla vel tristique porta.
 
@@ -77,8 +77,8 @@ h2. Sed vel ultrices ipsum
 2. Nullam eget neque quis justo euismod eleifend.
 3. Nullam quis turpis porttitor, elementum ex vitae, ultrices arcu.
 4. Aliquam imperdiet ex ut purus tincidunt pretium.`,
-    // 'AsciiDoc': ``,
-    'Creole': `= Sample Creole document =
+    // AsciiDoc: ``,
+    Creole: `= Sample Creole document =
 
 **Lorem ipsum** dolor sit amet, consectetur adipiscing elit. Nunc vitae lorem molestie, tristique diam at, egestas neque. Nam a interdum urna, eget sodales quam. Vestibulum venenatis libero id dui iaculis sagittis. Vestibulum vel iaculis justo, in posuere ligula. In lacinia massa ac ultricies pretium. Phasellus hendrerit consectetur leo, non pharetra quam hendrerit vel. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque id sem maximus, tempor massa eu, gravida turpis. Aliquam nec risus semper dui vulputate commodo eu vitae massa. In fringilla nulla vel tristique porta.
 
@@ -96,7 +96,7 @@ h2. Sed vel ultrices ipsum
 # Nullam eget neque quis justo euismod eleifend.
 # Nullam quis turpis porttitor, elementum ex vitae, ultrices arcu.
 # Aliquam imperdiet ex ut purus tincidunt pretium.`,
-    'BBCode': `[style size=36]Sample BBCode document[/style]
+    BBCode: `[style size=36]Sample BBCode document[/style]
 
 [b]Lorem ipsum[/b] dolor sit amet, consectetur adipiscing elit. Nunc vitae lorem molestie, tristique diam at, egestas neque. Nam a interdum urna, eget sodales quam. Vestibulum venenatis libero id dui iaculis sagittis. Vestibulum vel iaculis justo, in posuere ligula. In lacinia massa ac ultricies pretium. Phasellus hendrerit consectetur leo, non pharetra quam hendrerit vel. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque id sem maximus, tempor massa eu, gravida turpis. Aliquam nec risus semper dui vulputate commodo eu vitae massa. In fringilla nulla vel tristique porta.
 
