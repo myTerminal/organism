1cae584f8df93b5ef111f1e7248a39a887aad9c8
myTerminal <team.terminal@gmail.com>
2018-08-20 16:02:48 -0500

    add a few tests

--- src/scripts/components/input.test.jsx
+++ src/scripts/components/input.test.jsx
@@ -1,16 +1,45 @@
-/* global describe it */
+/* global describe it afterEach */
 
 import React from 'react';
-import TestUtils from 'react-dom/test-utils';
+import { cleanup, render, fireEvent } from 'react-testing-library';
+import sinon from 'sinon';
 import Input from './input.jsx';
 
-const should = require('chai').should();
+const should = require('chai').should(),
+    noOperation = function () {};
 
 describe('input', function () {
-    it('renders without problems', function () {
-        const noOperation = function () {},
-            input = TestUtils.renderIntoDocument(<Input value="" onChange={noOperation} />);
+    afterEach(cleanup);
 
-        should.exist(input);
+    it('renders without problems with empty props', function () {
+        const { container } = render(<Input text="" onChange={noOperation} />);
+        const textArea = container.querySelector('textarea');
+
+        should.exist(textArea);
+    });
+
+    it('renders text received through props', function () {
+        const { container } = render(<Input text="Sample text" onChange={noOperation} />);
+        const textArea = container.querySelector('textarea');
+
+        textArea.innerHTML.should.equal('Sample text');
+    });
+
+    it.skip('reports text change', function () {
+        const onTextChange = sinon.fake(),
+            eventData = {
+                target: {
+                    value: 'New Text'
+                }
+            };
+
+        const { container } = render(<Input text="Sample text" onChange={onTextChange} />);
+        const textArea = container.querySelector('textarea');
+
+        textArea.value = 'New Text';
+        fireEvent.change(textArea);
+
+        onTextChange.called.should.equal(true);
+        onTextChange.calledWith(eventData).should.equal(true);
     });
 });
---
+++ src/scripts/components/output.test.jsx
@@ -0,0 +1,27 @@
+/* global describe it afterEach */
+
+import React from 'react';
+import { cleanup, render } from 'react-testing-library';
+import sinon from 'sinon';
+import Output from './output.jsx';
+
+const should = require('chai').should(),
+    noOperation = function () {};
+
+describe('output', function () {
+    afterEach(cleanup);
+
+    it('renders without problems with empty props', function () {
+        const { container } = render(<Output text="" />);
+        const outputDiv = container.querySelector('.output');
+
+        should.exist(outputDiv);
+    });
+
+    it('renders text received through props', function () {
+        const { container } = render(<Output text="<h1>Sample text</h1>" />);
+        const outputDiv = container.querySelector('.output');
+
+        outputDiv.innerHTML.should.equal('<h1>Sample text</h1>');
+    });
+});
