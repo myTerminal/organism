84844994405901aa23c362440c036d35f23ad489
myTerminal <team.terminal@gmail.com>
2018-09-29 21:00:36 -0500

    introduce cypress for functional testing

---
+++ cypress/integration/simple_spec.js
@@ -0,0 +1,45 @@
+describe('Simple UI tests for organism', function () {
+    beforeEach(function () {
+        cy.visit('http://localhost:8080');
+    });
+
+    it('Renders the application correctly in landscape mode', function () {
+        cy.get('.header')
+            .should('be.visible');
+
+        cy.get('.input')
+            .should('be.visible');
+
+        cy.get('.output')
+            .should('be.visible');
+
+        cy.get('.footer')
+            .should('be.visible');
+    });
+
+    it('Renders the application correctly in portrait mode', function () {
+        cy.viewport(420, 480);
+
+        cy.get('.header')
+            .should('be.visible');
+
+        cy.get('.input')
+            .should('be.visible');
+
+        cy.get('.output')
+            .should('be.visible');
+
+        cy.get('.footer')
+            .should('be.visible');
+    });
+
+    it('Changing the input text changes the text on the right while in org mode', function () {
+        cy.get('.input')
+            .type('{selectall}{backspace}')
+            .type('#+TITLE: This is the title of the document');
+
+        cy.get('.output')
+            .invoke('html')
+            .should('include', '<h1>This is the title of the document</h1>');
+    });
+});
