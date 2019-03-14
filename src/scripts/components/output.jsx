ccb301b745bde4a83edda73071e235e272116611
myTerminal <team.terminal@gmail.com>
2019-03-13 22:03:31 -0500

    beautify output using github-markdown.css

--- src/scripts/components/output.jsx
+++ src/scripts/components/output.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Output extends React.Component {
     render() {
         return (
-            <div className="output"
+            <div className="output markdown-body"
                 dangerouslySetInnerHTML={{ __html: this.props.text }}
             />
         );
