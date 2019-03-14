0e57e818bf113e171755262e2a7a9f48953dd85c
myTerminal <team.terminal@gmail.com>
2019-03-13 22:08:20 -0500

    beautify output using github-markdown.css

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* global caches fetch skipWaiting */
 
-var cacheName = '1547707540571',
+var cacheName = '1552532671049',
     origin = '/organism/';
 
 this.addEventListener('install', function (event) {
